8e6fb3bb18ec16fa7a304c500a74d726ab27dfb7
ASUS <ASUS@192.168.1.11>
2026-04-21 17:56:43 +0300

    Initial commit

---
+++ PlateformeElearning/src/main/java/com/fst/elearning/PlateformeElearningApplication.java
@@ -0,0 +1,13 @@
+package com.fst.elearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlateformeElearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlateformeElearningApplication.class, args);
+	}
+
+}
